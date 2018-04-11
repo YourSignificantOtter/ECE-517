78d5a8c5e0a8e272076465b40dc1f07a0905fa07
Nick Herrmann <nicholas.herrmann94@gmail.com>
2018-04-11 10:19:23 -0400

    Removed tsys

--- rats/db/seeds.rb
+++ rats/db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create(email: 'admin@tsys.com',password: '1qazxsw2',name: 'Admin I. Strator', company_id: nil, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
+User.create(email: 'admin@temporary.com',password: '1qazxsw2',name: 'Admin I. Strator', company_id: nil, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
 #Seeding the Role table
 [:admin, :recruiter, :job_seeker].each do |role|
   Role.where({ name: role }, without_protection: true).first_or_create
@@ -20,13 +20,13 @@ User.first.add_role(:admin)
 Company.destroy_all
 Company.create!(name: "Company 1", headquarters: "Palo Alto, TX", website: "http://www.company.com", size: "1,000+", synopsis: "We do things for money", founded: 1999, industry: "Computer", revenue: "$100 million")
 
-User.create!(email: 'recruiter@tsys.com',password: 'pass',name: 'First Recruiter', company_id: Company.find_by_name("Company 1").id, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
-User.find_by_email('recruiter@tsys.com').add_role(:recruiter)
-User.create(email: 'job_seeker@tsys.com',password: 'pass',name: 'First Seeker', company_id: nil, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
-User.find_by_email('job_seeker@tsys.com').add_role(:job_seeker)
-User.create(email: 'both@tsys.com',password: 'pass',name: 'Recruiter Seeker', company_id: Company.find_by_name("Company 1").id, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
-User.find_by_email('both@tsys.com').add_role(:job_seeker)
-User.find_by_email('both@tsys.com').add_role(:recruiter)
+User.create!(email: 'recruiter@temporary.com',password: 'pass',name: 'First Recruiter', company_id: Company.find_by_name("Company 1").id, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
+User.find_by_email('recruiter@temporary.com').add_role(:recruiter)
+User.create(email: 'job_seeker@temporary.com',password: 'pass',name: 'First Seeker', company_id: nil, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
+User.find_by_email('job_seeker@temporary.com').add_role(:job_seeker)
+User.create(email: 'both@temporary.com',password: 'pass',name: 'Recruiter Seeker', company_id: Company.find_by_name("Company 1").id, address: '123 NCSU Drive', city: 'Raleigh', state: 'NC', country: 'USA', phone_number: '555-555-5555')
+User.find_by_email('both@temporary.com').add_role(:job_seeker)
+User.find_by_email('both@temporary.com').add_role(:recruiter)
 
 Job.destroy_all
-Job.create(job_title: "Worker 1", company_id: Company.find_by_name("Company 1").id, employment_type: "Full Time", job_description: "Do work for us", requirements: "Must be 6 feet tall", responsibilities: "Work hard", user_id: User.find_by_email('recruiter@tsys.com').id)
+Job.create(job_title: "Worker 1", company_id: Company.find_by_name("Company 1").id, employment_type: "Full Time", job_description: "Do work for us", requirements: "Must be 6 feet tall", responsibilities: "Work hard", user_id: User.find_by_email('recruiter@temporary.com').id)
